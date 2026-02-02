44c7ae4849da70d0e7b8f8ad81464f814c238a59
Sarath-33 <withsarath.m@gmail.com>
2026-02-02 21:21:43 +0530

    completed plan controller

--- server/src/controllers/planController.js
+++ server/src/controllers/planController.js
@@ -1,12 +1,58 @@
-export const getAllPlans = async (req,res) =>{
-    
-}
-export const getPlan = async (req,res) =>{
+import Plan from "../models/planModel"
 
+export const getAllPlans = async (req, res) => {
+    try {
+        const plans = await Plan.find({ isActive: true }).sort({ createdAt: -1 });
+    res.status(200).json(plans);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!!", error })
+    }
 }
-export const createPlan = async (req,res) =>{
+export const getPlan = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const plan = await Plan.findById(id);
+        if(!plan) {
+            return res.status(404).json({message: "Not Found"})
+        }
+        res.status(200).json(plan)
 
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!!", error })
+    }
 }
-export const updatePlan = async (req,res) =>{
+export const createPlan = async (req, res) => {
+    try {
+        const { name, price, duration } = req.body;
+        if (!name || !price || !duration) {
+            return res.status(400).json({ message: "All fields are required!!" })
+        }
+        const plan = new Plan({
+            name,
+            price,
+            duration,
+            createdBy: req.user.id
+        })
+        await plan.save();
+        res.status(201).json(plan);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!!", error })
+    }
+}
+export const updatePlan = async (req, res) => {
+    try {
+        const {name, price, duration} =  req.body
+        const updatedPlan = await Plan.findByIdAndUpdate(req.params.id,{
+            name,
+            price,
+            duration
+        }, {new: true})
 
+        if(!updatedPlan) {
+            return res.status(400).json({message: "no plan found!"})
+        }
+        res.status(200).json(updatedPlan)
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!!", error })
+    }
 }
