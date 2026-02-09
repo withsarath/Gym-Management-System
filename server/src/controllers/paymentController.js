add4a26b51117cb837bc84071f54a4347e7b496b
Sarath-33 <withsarath.m@gmail.com>
2026-02-09 13:36:37 +0530

    completed payment model and routes

---
+++ server/src/controllers/paymentController.js
@@ -0,0 +1,67 @@
+import Member from "../models/memberModel.js"
+import Plan from "../models/planModel.js"
+import Payment from "../models/paymentModel.js"
+import Subscription from "../models/subscriptionModel.js"
+
+export const makePayment = async (req, res) => {
+    try {
+        const { memberId, planId, method } = req.body;
+        if (!memberId || !planId || !method) {
+            return res.status(400).json({ message: "All feilds are required" })
+        }
+
+        const member = await Member.findById(memberId);
+        if (!member) {
+            return res.status(404).json({ message: "Member not found!" })
+        }
+
+        const plan = await Plan.findById(planId);
+        if (!plan) {
+            return res.status(404).json({ message: "Plan not found!" })
+        }
+
+        const payment = new Payment({
+            member: memberId,
+            plan: planId,
+            amount: plan.price, method,
+            status: "paid"
+        });
+
+        await payment.save();
+
+        let subscription = await Subscription.findOne({ member: memberId });
+        if (!subscription) {
+            subscription = new Subscription({
+                member: memberId,
+                plan: planId,
+                startDate: new Date(),
+                endDate: new Date(Date.now() + plan.duration * 24 * 60 * 60 * 1000),
+                status: "active",
+                createdBy: req.user.id
+            })
+            await subscription.save();
+        }
+        res.status(201).json({
+            message: "Payment successful",
+            payment,
+            subscription
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error });
+    }
+}
+export const getPayments = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error });
+    }
+}
+export const memberPayments = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error });
+    }
+}
