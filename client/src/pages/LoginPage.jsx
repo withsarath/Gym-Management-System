305a280192cd4814209fb38e31385f64d8623a14
Sarath-33 <withsarath.m@gmail.com>
2026-02-13 13:17:03 +0530

    setup pages

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+
+const LoginPage = () => {
+  return (
+    <div className="text-center">
+      <h2>Login</h2>
+      <label htmlFor="">Email</label>
+      <br />
+      <input type="email" placeholder="eg: email@gmail.com" />
+      <br />
+      <label htmlFor="">password</label>
+      <br />
+      <input type="password" placeholder="*******"/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/pages/MembersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MembersPage = () => {
+  return (
+    <div>
+      members page
+    </div>
+  )
+}
+
+export default MembersPage
