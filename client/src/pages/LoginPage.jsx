281bd62b54d516ad5a0677e06850ab89c8a7dbbe
Sarath-33 <withsarath.m@gmail.com>
2026-02-19 22:00:10 +0530

    Worked on loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,17 +1,76 @@
+import { useState } from "react";
+import axios from "axios";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError("");
+
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+
+      console.log("Login Success:", res.data);
+
+      // Save token
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful ✅");
+    } catch (err) {
+      setError("Invalid email or password",err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="text-center">
       <h2>Login</h2>
-      <label htmlFor="">Email</label>
-      <br />
-      <input type="email" placeholder="eg: email@gmail.com" />
-      <br />
-      <label htmlFor="">password</label>
-      <br />
-      <input type="password" placeholder="*******"/>
-    </div>
-  )
-}
 
-export default LoginPage
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            placeholder="eg: email@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
