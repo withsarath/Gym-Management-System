5ef6da54aa473e4f0e4ed2914c663ab59b0f99ec
Sarath-33 <withsarath.m@gmail.com>
2026-02-20 22:11:19 +0530

    fixed some bugs and added protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,34 @@
-import { Route, Routes } from "react-router-dom"
-import LoginPage from "./pages/LoginPage"
-import Dashboard from "./pages/Dashboard"
-import MembersPage from "./pages/MembersPage"
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import MembersPage from "./pages/MembersPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <>
       <h1>Gym Management System</h1>
       <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/members" element={<MembersPage/>}/>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <LoginPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/members" element={<MembersPage />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,6 +25,7 @@ const LoginPage = () => {
 
       // Save token
       localStorage.setItem("token", res.data.token);
+      navigate("/dashboard");
 
       alert("Login Successful ✅");
     } catch (err) {
